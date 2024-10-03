d213790c95cb4ecfc3d3a7fd618a9b8d4e779803
root <root@STAR>
2024-10-03 22:50:08 +0300

    EvCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,17 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color); // Call the parent constructor
-    this._range = range; // Add the new attribute for EVCar
+    super(brand, motor, color);
+    this._range = range;
   }
 
   static get [Symbol.species]() {
-    return Car; // Specify that the species for cloning should return Car
-  }
-
-  cloneCar() {
-    // Use the Symbol.species to create a new instance of Car
-    return new (this.constructor[Symbol.species])(this._brand, this._motor, this._color);
+    return Car;
   }
 }
