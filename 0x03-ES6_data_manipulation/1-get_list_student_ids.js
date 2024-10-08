3171f9bf0356f782d51ee8f298c7d9681ef83d4a
root <root@STAR>
2024-10-08 21:03:04 +0300

    mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(arr) {
   // Initialize newArr as an empty array
   let newArr = [];
-
   // Check if arr is an instance of Array
   if (arr instanceof Array) {
     // Use map to extract ids from the array of student objects
