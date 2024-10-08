7eb394bd1ce74d109e0475222a72a46ca168bc1c
root <root@STAR>
2024-10-08 21:56:31 +0300

    filter and map combined

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,26 +1,22 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // Filter students based on the specified city
+  // Filter students by the specified city
   const filterStudents = students.filter(
     (student) => student.location === city,
   );
 
-  // Map over the filtered students and update their grades
-  const gradeStudents = filterStudents.map((student) => {
-    // Find the corresponding grade for each student
-    for (const gradeInfo of newGrades) {
-      if (student.id === gradeInfo.studentId) {
-        student.grade = gradeInfo.grade; // Update grade if found
-      }
-    }
+  // Map through the filtered students and update their grades
+  const gradeStudents = filterStudents.map(
+    (student) => {
+      const studentGrade = newGrades.find((gradeInfo) => student.id === gradeInfo.studentId);
 
-    // If no grade is found, set grade to 'N/A'
-    if (student.grade === undefined) {
-      student.grade = 'N/A'; // eslint-disable-line no-param-reassign
-    }
+      // If a grade is found, assign it; otherwise, set it to 'N/A'
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : 'N/A',
+      };
+    },
+  );
 
-    return student; // Return the student with updated grade
-  });
-
-  // Return the updated list of students
+  // Return the array of students with updated grades
   return gradeStudents;
 }
